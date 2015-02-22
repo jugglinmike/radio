14c19db17918ae6735d0bd330e4aee83c0f245fe
Mike Pennisi <mike@mikepennisi.com>
2015-02-22 18:43:37 -0500

    Generalize user account
    Since the goal is to run on a real device, I want to avoid assuming a
    Vagrant environment wherever possible.

--- provision.sh
+++ provision.sh
@@ -2,14 +2,29 @@
 
 set -e
 
+PROJECT_DIR=/vagrant
+USER=radio
+
 export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
-sudo usermod -a -G audio vagrant
-sudo apt-get install -y alsa alsa-utils pulseaudio espeak
+sudo apt-get install -y alsa alsa-utils pulseaudio espeak mpd mpc
+
+useradd -d /home/$USER -m $USER
+sudo usermod -a -G audio $USER
 
 amixer sset Master unmute
 amixer sset Master 100%
+amixer sset "Master Mono" unmute
+amixer sset "Master Mono" 100%
 amixer sset PCM unmute
 amixer sset PCM 100%
 
+echo "Configuring Music Player Daemon"
+sed "s/{{\s*USER\s*}}/$USER/g" $PROJECT_DIR/src/system/mpd.conf > /etc/mpd.conf
+mkdir -p /home/$USER/music
+chown $USER /home/$USER/music
+chown -R $USER /var/run/mpd
+service mpd restart
+
+su $USER
 espeak "Hello World"
